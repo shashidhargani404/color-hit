fcc77325eb8b9b58c2cc0fde148afb8adece4080
shashidhargani404 <shashidhargani404@gmail.com>
2020-01-24 11:58:12 +0530

    commit

---
+++ build/precache-manifest.502f66419906f6521771a47633c5a567.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5a1f3ef0e7a4f7655999a5afafacfc9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3291c0513baa130415e7",
+    "url": "/static/css/main.80304eac.chunk.css"
+  },
+  {
+    "revision": "6db4c0701c795a69aa9d",
+    "url": "/static/js/2.dedda4ff.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.dedda4ff.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3291c0513baa130415e7",
+    "url": "/static/js/main.1e9dbd92.chunk.js"
+  },
+  {
+    "revision": "e6e78466b638df2e3d68",
+    "url": "/static/js/runtime-main.ef29f233.js"
+  }
+]);
